971af4054ab2488cc41093ce0940adc419bb6b0e
tornado76 <oscaraigf@gmail.com>
2016-05-15 16:22:13 +0200

    Squisher/Opticharger (2/2)

--- configs/nexus_main.mk
+++ configs/nexus_main.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Scripts 
+SQUISHER_SCRIPT := vendor/nexus/tools/squisher
+
 # Include overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/nexus/overlay/common
